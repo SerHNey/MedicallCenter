f506be7cdb0aee2b79a3a21b3904d81e2539b676
Нечто Страшное <naumov-1212@Mail.ru>
2023-03-02 12:28:04 +0300

    Добавьте файлы проекта.

---
+++ MedicallCenter/Pages/Page_Authorization.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MedicalCenter.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Page_Authorization : Page
+    {
+        public Page_Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void cbShowPassword_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbShowPassword.IsChecked == true)
+            {
+                tboxPassword.Text = pboxPassword.Password;
+
+                pboxPassword.Visibility = Visibility.Hidden;
+                tboxPassword.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                pboxPassword.Visibility = Visibility.Visible;
+                tboxPassword.Visibility = Visibility.Hidden;
+            }
+            
+        }
+    }
+}
---
+++ MedicallCenter/Pages/Page_Home.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MedicalCenter.Pages;
+
+namespace MedicalCenter.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Home.xaml
+    /// </summary>
+    public partial class Page_Home : Page
+    {
+        public Page_Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button_service_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.Navigate(new Page_Servicee());
+        }
+
+        private void button_worker_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.Navigate(new Page_Workers());
+        }
+
+        private void button_user_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.Navigate(new Page_Users());
+        }
+
+        private void button_result_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.Navigate(new Page_Result());
+        }
+    }
+}
