8cdfcc121a24fd9c0181d5680303c32efdc1e729
ермолаевас <@208-08>
2023-03-06 14:48:09 +0300

    asd

--- MedicallCenter/Pages/Page_Workers.xaml.cs
+++ MedicallCenter/Pages/Page_Workers.xaml.cs
@@ -25,122 +25,40 @@ namespace MedicalCenter.Pages
     /// </summary>
     public partial class Page_Workers : Page
     {
-        private Worker curerentworker = new Worker();
+        private int pageNumber = 0;
+        private int maxpage = 0;
+        private int pageSize = 20;
+        List<Worker> workers = new List<Worker>();
         public Page_Workers()
         {
             InitializeComponent();
-
-            DataGridWorkers.ItemsSource = CurrentData.db.Worker.ToList();
-            ComboService.ItemsSource = CurrentData.db.Service.ToList();
-            ComboDolgnost.ItemsSource = CurrentData.db.Type.ToList();
+            workers = CurrentData.workers;
+            maxpage = workers.Count / pageSize;
+            DisplayDataInGrid();
+        }
+        private void DisplayDataInGrid()
+        { 
+            var currentPageData = workers.Skip(pageNumber * pageSize).Take(pageSize); // отображаем только данные для текущей страницы
+            DataGridWorkers.ItemsSource = currentPageData; // отображаем данные в DataGrid
         }
 
-        private void btnBack_Click(object sender, RoutedEventArgs e)
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
         {
-            Manager.frame.Navigate(new Page_Home(CurrentData.worker));
-        }
-
-        private void btnDelete_Click(object sender, RoutedEventArgs e)
-        {
-            var workersForDelete = DataGridWorkers.SelectedItems.Cast<Worker>().ToList();
-            if (MessageBox.Show($"Вы точно хотите удалить следующие записи {workersForDelete.Count()} элементов","Внимание",
-                MessageBoxButton.YesNo,MessageBoxImage.Question)== MessageBoxResult.Yes)
+            if(pageNumber < maxpage)
             {
-                try
-                {
-                    CurrentData.db.Worker.RemoveRange(workersForDelete);
-                    CurrentData.db.SaveChanges();
-                    DataGridWorkers.ItemsSource = CurrentData.db.Worker.ToList();
-                }
-                catch(Exception ex)
-                {
-                    MessageBox.Show(ex.ToString());
-                }
+                pageNumber++; // переход на следующую страницу
+                DisplayDataInGrid(); // отображение данных
             }
         }
 
-        private void btnAddWorker_Click(object sender, RoutedEventArgs e)
+        private void PreviousPageButton_Click(object sender, RoutedEventArgs e)
         {
-            if (GetData())
+            if (pageNumber > 0)
             {
-                CurrentData.db.Worker.Add(curerentworker);
-                SaveChang();
-                MessageBox.Show("Добавлено");
+                pageNumber--; // переход на предыдущую страницу
+                DisplayDataInGrid(); // отображение данных
             }
         }
-
-        private void btnEditWorker_Click(object sender, RoutedEventArgs e)
-        {
-            if(DataGridWorkers.SelectedItem != null)
-            {
-
-                curerentworker = DataGridWorkers.SelectedItem as Worker;
-                tbNameWorker.Text = curerentworker.name;
-                tbLoginWorker.Text = curerentworker.login;
-                tbPasswordWorker.Text = curerentworker.password;
-                tbIpWorker.Text = curerentworker.ip;
-                tbLasneterWorker.Text = curerentworker.lastenter;
-                JArray json = JArray.Parse(curerentworker.services);
-                List<Service> cur_services = new List<Service>();
-                foreach (JObject jobj in json)
-                {
-                    cur_services.Add(CurrentData.services.FirstOrDefault(n=>n.kod_service == (int)jobj["code"]));
-                }
-                ComboService.ItemsSource = cur_services;
-
-                btnEditSecondWorker.Visibility = Visibility.Visible;
-            }
-
-        }
-
-        private void btnEditSecondWorker_Click(object sender, RoutedEventArgs e)
-        {
-            if(GetData())
-            {
-                CurrentData.db.Worker.AddOrUpdate(curerentworker);
-                SaveChang();
-
-                MessageBox.Show("Запись успешно изменена");
-                btnEditSecondWorker.Visibility = Visibility.Hidden;
-                btnEditWorker.IsEnabled = IsEnabled.Equals(true);
-            }
-        }
-        private void SaveChang()
-        {
-            CurrentData.db.SaveChanges();
-            DataGridWorkers.ItemsSource = CurrentData.db.Worker.ToList();
-        }
-        private bool GetData()
-        {
-            StringBuilder stringBuilder= new StringBuilder();
-            if(tbNameWorker.Text.Length < 0) 
-                stringBuilder.Append("Поле имя: пусто\n");
-            if (tbLoginWorker.Text.Length < 0)
-                stringBuilder.Append("Поле логин: пусто\n");
-            if (tbPasswordWorker.Text.Length < 0)
-                stringBuilder.Append("Поле пароль: пусто\n");
-            if (tbIpWorker.Text.Length < 0)
-                stringBuilder.Append("Поле ip: пусто\n");
-            if (tbLasneterWorker.Text.Length < 0)
-                stringBuilder.Append("Поле время: пусто\n");
-            if (ComboService.SelectedItem == null)
-                stringBuilder.Append("Поле услуги: пусто\n");
-            if (ComboDolgnost.SelectedItem == null)
-                stringBuilder.Append("Поле должность: пусто\n");
-            if(stringBuilder.ToString() == "")
-            {
-                curerentworker.name = tbNameWorker.Text;
-                curerentworker.login = tbLoginWorker.Text;
-                curerentworker.password = tbPasswordWorker.Text;
-                curerentworker.ip = tbIpWorker.Text;
-                curerentworker.lastenter = tbLasneterWorker.Text;
-                curerentworker.services = ComboService.SelectedValue.ToString();
-                curerentworker.type = CurrentData.db.Type.ToList().Where(x => x.role == ComboDolgnost.Text).FirstOrDefault().id;
-                return true;
-            }
-            return false;
-        }
-
         private void search_GotFocus(object sender, RoutedEventArgs e)
         {
             if (search.Text == "Поиск")
@@ -152,18 +70,30 @@ namespace MedicalCenter.Pages
         {
             if (search.Text != "" && DataGridWorkers != null)
             {
-                var filterName = CurrentData.workers.Where(n=>n.name.ToLower().Contains(search.Text.ToLower()));
-                DataGridWorkers.ItemsSource = filterName;
+                workers = workers.Where(n=>n.name.ToLower().Contains(search.Text.ToLower())).ToList();
+                DataGridWorkers.ItemsSource = workers;
+                maxpage = workers.Count / pageSize;
             }
             else
             {
                 if (DataGridWorkers != null)
                 {
-                    DataGridWorkers.ItemsSource = CurrentData.workers;
+                    workers = CurrentData.workers;
+                    maxpage = workers.Count / pageSize;
                 }
 
             }
         }
 
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Worker worker = DataGridWorkers.SelectedValue as Worker;
+            Manager.frame.Navigate(new Page_EditWorkers(worker));
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.frame.GoBack();
+        }
     }
 }
